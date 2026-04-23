0200e50317275e15190b85f4822a943b9522331f
Ananya Agarwal <ananya.2018@icloud.com>
2026-04-23 15:59:38 -0400

    fixed assets'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "myportfolio",
+  base: "/myportfolio/",
   server: {
     port: 5173,
   },
